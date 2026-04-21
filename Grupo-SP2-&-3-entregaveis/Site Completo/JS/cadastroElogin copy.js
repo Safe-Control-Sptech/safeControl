f67c49fd4df2a61fd55973668c5cbc6e6cf665b0
Leogalfaro <leo@MacBook-Pro-de-Leonardo.local>
2026-04-21 17:40:31 -0300

    Finalizando JS telas login e cadastro

---
+++ Grupo-SP2-&-3-entregaveis/Site Completo/JS/cadastroElogin copy.js
@@ -0,0 +1,35 @@
+    emails = []
+    senhas = []
+    
+    function entrar() {
+        let login = ipt_email.value;
+        let senha = ipt_senha.value;
+
+        if (emails.includes(login) || senhas.includes(senha)) {
+            alert ('email/senha está(ão) incorreto(os)');
+        } else {
+            alert ('Você será redirecionado. AGUARDE!');
+            window.location.replace("./dashboard.html");
+        }
+    }
+
+
+    function Criar() {
+        let emailcad = ipt_email.value;
+        let senha = ipt_senha.value;
+        let senha_rep = ipt_confirme.value;
+
+        if ((emailcad.indexOf('@gmail')  == -1 || emailcad.indexOf('@uol')  == -1 || emailcad.indexOf('@outlook')  == -1) &&
+                                                                        emailcad.indexOf('.com', '.org', '.com.br', '.net') == -1) {
+            alert ('digite um E-mail válido');
+        } if (senha.length < 7) {
+            alert ('A senha deve conter no minimo 7 digitos');
+        } if (senha_rep != senha) {
+            alert ('As senhas não conhecidem');
+        } else {
+            emails.push(emailcad);
+            senhas.push(senha);
+            window.location.href = "./login.html";
+        }
+
+    }
--- Grupo-SP2-&-3-entregaveis/Site Completo/JS/cadastroElogin.js
+++ Grupo-SP2-&-3-entregaveis/Site Completo/JS/cadastroElogin.js
@@ -1,23 +1,26 @@
-    email = []
-    senha = []
+    emails = []
+    senhas = []
     
     function entrar() {
-        let login = ipt_login.value;
+        let login = ipt_email.value;
         let senha = ipt_senha.value;
-
-        if (login != 'sptech@sptech.school' || senha != 'Senha@123') {
+             
+        if (emails.includes(login) || senhas.includes(senha)) {
             alert ('email/senha está(ão) incorreto(os)');
+
+                
         } else {
             alert ('Você será redirecionado. AGUARDE!');
-            window.location.replace("https://www.google.com")
+            window.location.replace("./dashboard.html");
         }
+        
+    
     }
-
 
     function Criar() {
         let emailcad = ipt_email.value;
         let senha = ipt_senha.value;
-        let senha_rep = ipt_senha_rep.value;
+        let senha_rep = ipt_confirme.value;
 
         if ((emailcad.indexOf('@gmail')  == -1 || emailcad.indexOf('@uol')  == -1 || emailcad.indexOf('@outlook')  == -1) &&
                                                                         emailcad.indexOf('.com', '.org', '.com.br', '.net') == -1) {
@@ -27,9 +30,9 @@
         } if (senha_rep != senha) {
             alert ('As senhas não conhecidem');
         } else {
-            email.push(email.cad);
-            senha.push(senha);
-            window.location.href = "TELA_login_versaobase.html"
+            emails.push(emailcad);
+            senhas.push(senha);
+            window.location.href = "./login.html";
         }
 
     }
