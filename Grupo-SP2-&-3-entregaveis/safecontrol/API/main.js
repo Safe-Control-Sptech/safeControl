8c0a8b50a591d899e567e8490a874b59e86f8a1b
Luizjoaquim <luizjoaquimsobrinho@gmail.com>
2026-04-24 15:41:48 -0300

    adicionando configurações do main.js

--- Grupo-SP2-&-3-entregaveis/safecontrol/API/main.js
+++ Grupo-SP2-&-3-entregaveis/safecontrol/API/main.js
@@ -8,7 +8,7 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (
@@ -19,11 +19,11 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: 'HOST_DO_BANCO',
-            user: 'USUARIO_DO_BANCO',
-            password: 'SENHA_DO_BANCO',
-            database: 'DATABASE_DO_BANCO',
-            port: 3306
+            host: '127.0.0.1',
+            user: 'usuario_insert',
+            password: 'Safecontrol123@',
+            database: 'safeControl',
+            port: 3307
         }
     ).promise();
 
@@ -63,7 +63,7 @@ const serial = async (
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO medida (sensor_analogico, sensor_digital) VALUES (?, ?)',
+                'INSERT INTO captura (temperatura, umidade, fkSensor, fkUnidadeTemp, fkUnidadeUmi) VALUES (?, ?, 1, 1, 2);',
                 [sensorAnalogico, sensorDigital]
             );
             console.log("valores inseridos no banco: ", sensorAnalogico + ", " + sensorDigital);
