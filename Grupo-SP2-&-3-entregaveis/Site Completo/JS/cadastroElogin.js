f47f56b4ba74fb517f84d1d46089ce8ef4327cc3
Arthur-Balduino <arthur.balduino@sptech.school>
2026-05-03 00:01:55 -0300

    ADD: Validação com FOR no Login e Atualização CSS Cadastro e Login

--- Grupo-SP2-&-3-entregaveis/Site Completo/JS/cadastroElogin.js
+++ Grupo-SP2-&-3-entregaveis/Site Completo/JS/cadastroElogin.js
@@ -1,32 +1,36 @@
 let emails = ['admin', 'aluno@sptech.school'];
 let senhas = ['admin', 'aluno.sptech'];
 
-console.log(emails, senhas)
+console.log(emails, senhas);
+
+let tentativas = 0
 // login
 function entrar() {
     let login = ipt_email.value;
     let senha = ipt_senha.value;
 
-    let tentativas = 0
-    let validacao = true;
+    let validacao = false;
+    let ax_erro = false;
 
-    for (let i = 0; i <= emails.length; i++) {
-        if ((login[i] == 'admin' && senha[i] == 'admin') ||
-            (login[i] == 'aluno@sptech.school' && senha[i] == 'aluno.sptech')){
-            validacao = true;
-            window.location.replace("./dashboard.html");
-        } else {
-            validacao = false;
-        }
-    }
-
-    if (!validacao) {
-        tentativas++;
-        msg_Erro.innerHTML = `Tentativa ${tentativas}: Email/senha incorretos`;
-    }
-
-    if (tentativas == 3) {
+    if (tentativas >= 3) {
         msg_Erro.innerHTML += `<br>Limite de tentativas atingido!`;
+        ax_erro = true;
+        console.log(ax_erro);
+    }
+
+    if (ax_erro == false) {
+        for (let i = 0; i < emails.length; i++) {
+            if (login == emails[i] && senha == senhas[i]) {
+                validacao = true;
+                window.location.href = "./dashboard.html";
+                break;
+            }
+        }
+
+        if (!validacao) {
+            tentativas++;
+            msg_Erro.innerHTML = `Tentativa ${tentativas}: Email/senha incorretos`;
+        }
     }
 }
 // cadastro
@@ -36,17 +40,35 @@ function Criar() {
     let senha = ipt_senha.value;
     let senha_rep = ipt_confirme.value;
 
-    if ((emailcad.indexOf('@gmail') == -1 || emailcad.indexOf('@uol') == -1 || emailcad.indexOf('@outlook') == -1) &&
-        emailcad.indexOf('.com', '.org', '.com.br', '.net') == -1 || emailcad == '') {
-        msg_Erro.innerHTML = 'digite um E-mail válido';
-    } if (senha.length < 7 || senha == '' || senha == '') {
-        msg_Erro.innerHTML = 'A senha deve conter no minimo 7 digitos';
-    } if (senha_rep != senha) {
-        msg_Erro.innerHTML = 'As senhas não conhecidem';
+    let ax_erro = false;
+
+    msg_Erro.innerHTML = '';
+
+    if (emailcad == "" ||
+        (!emailcad.includes('@gmail') &&
+            !emailcad.includes('@uol') &&
+            !emailcad.includes('@outlook'))
+    ) {
+        ax_erro = true;
+        msg_Erro.innerHTML = 'Digite um e-mail válido';
     }
+
+    if (senha.length < 7 || senha == '' || senha_rep == '') {
+        ax_erro = true;
+        msg_Erro.innerHTML = 'A senha deve conter no minimo 7 caracteres';
+    }
+
+    if (senha_rep != senha) {
+        ax_erro = true;
+        msg_Erro.innerHTML = 'As senhas não coincidem';
+    }
+
     if (emailcad == "") {
+        ax_erro = true;
         msg_Erro.innerHTML = 'O campo de Email esta vazio';
-    } else {
+    }
+
+    if (ax_erro == false) {
         emails.push(emailcad);
         senhas.push(senha);
         window.location.href = "./login.html";
